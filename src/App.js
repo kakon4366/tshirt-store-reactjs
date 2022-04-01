c69bec945bb4a3ca78310a1f152d6ef98baef359
kakon4366 <kakonbarman4366@gmail.com>
2022-04-01 16:31:09 +0600

    Dark mode add success

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import TopHeader from "./Components/TopHeader/TopHeader";
 
 function App() {
+	const [dark, setDark] = useState("light");
+
+	const darkHandler = () => {
+		if (dark === "dark") {
+			setDark("light");
+		} else {
+			setDark("dark");
+		}
+	};
+
 	return (
-		<div>
-			<TopHeader></TopHeader>
+		<div className={dark}>
+			<TopHeader darkHandler={darkHandler} dark={dark}></TopHeader>
 			<Header></Header>
 		</div>
 	);
--- src/Components/TopHeader/TopHeader.js
+++ src/Components/TopHeader/TopHeader.js
@@ -1,29 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 
-const TopHeader = () => {
-	const [mode, setMode] = useState(true);
+const TopHeader = ({ dark, darkHandler }) => {
+	// const [mode, setMode] = useState(true);
 
-	const changeMode = () => {
-		setMode(!mode);
-	};
+	// const changeMode = () => {
+	// 	setMode(!mode);
+	// };
+
+	console.log(dark);
 
 	return (
-		<div className="border-b-4 py-1 bg-slate-200">
+		<div className="border-b-4 py-1 bg-slate-200 dark:bg-slate-900">
 			<div className="container mx-auto">
 				<div className="text-right">
-					<small className="mr-12">Email: kakonbarman4366@gmail.com</small>
-					<small className="mr-12">Phone: +8801303168572</small>
+					<small className="mr-12 dark:text-white">
+						Email: kakonbarman4366@gmail.com
+					</small>
+					<small className="mr-12 dark:text-white">
+						Phone: +8801303168572
+					</small>
 					<small>
-						{mode ? (
+						{dark === "light" ? (
 							<button
-								onClick={changeMode}
+								onClick={darkHandler}
 								className="border bg-black rounded text-white px-2"
 							>
 								dark
 							</button>
 						) : (
 							<button
-								onClick={changeMode}
+								onClick={darkHandler}
 								className="border bg-white text-black rounded px-2"
 							>
 								light
