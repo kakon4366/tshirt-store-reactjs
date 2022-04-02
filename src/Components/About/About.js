78b588f8ab6200ca67c840ea825fe9515eba0926
kakon4366 <kakonbarman4366@gmail.com>
2022-04-03 01:20:52 +0600

    About page update

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,10 +1,53 @@
 import React from "react";
+import profilePhoto from "../../images/kakon.jpg";
 
 const About = () => {
 	return (
-		<div>
-			<h1 className="text-4xl text-center">This is About Page</h1>
-		</div>
+		<section className="py-28 ">
+			<div className="container mx-auto">
+				<h1 className="text-4xl text-center uppercase font-semibold">
+					About Me
+				</h1>
+				<div className="mt-20 flex">
+					<img className="rounded w-[500px]" src={profilePhoto} alt="" />
+					<div className="ml-12">
+						<h2 className="text-4xl font-semibold">Kakon Barman</h2>
+						<h4 className="text-1xl">Web Developer</h4>
+						<br />
+						<p className="text-lg text-justify">
+							Hey, My name is Kakon Barman. I'm a Web Developer. Lorem
+							ipsum dolor sit amet consectetur adipisicing elit.
+							Inventore itaque veritatis ducimus sunt distinctio cum
+							nobis voluptatibus possimus suscipit doloremque repellat
+							laudantium nisi hic nihil dignissimos beatae sed, numquam
+							quod aliquid. Odit assumenda ratione nemo eos! Consectetur
+							similique repellat porro rem saepe, quibusdam est ad,
+							distinctio, magni autem ab eos.
+							<br />
+							<br />
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							Non dolor, architecto enim reprehenderit eum recusandae
+							omnis corrupti nostrum laudantium laborum aspernatur
+							aliquid consequuntur qui. Nobis aperiam, facilis assumenda
+							numquam laudantium at nemo veniam omnis a ea quibusdam,
+							dolorem voluptatem molestiae saepe ducimus sint deleniti
+							totam animi maiores, nulla autem! Vel, doloribus!
+							Dignissimos quia aspernatur, animi sunt beatae, ipsam
+							praesentium autem exercitationem aliquid doloremque quo
+							harum ducimus vel. Sed, accusantium? Reiciendis omnis culpa
+							impedit exercitationem voluptate! Illo, perferendis
+							repudiandae ipsam provident accusamus asperiores ullam
+							expedita magni laudantium sunt, nihil tenetur maxime. Dicta
+							amet quisquam pariatur adipisci recusandae laboriosam quos
+							et ab!
+						</p>
+						<button className="bg-blue-500 py-2 px-12 text-white rounded mt-8 hover:bg-blue-700 transition-all">
+							Learn More
+						</button>
+					</div>
+				</div>
+			</div>
+		</section>
 	);
 };
 
