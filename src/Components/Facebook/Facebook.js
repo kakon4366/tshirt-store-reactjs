7feb9ded3c62d0716f4815d3b5ae3de7ad7d5e14
kakon4366 <kakonbarman4366@gmail.com>
2022-04-03 16:33:06 +0600

    Facebook.js some bug fix

--- src/Components/Facebook/Facebook.js
+++ src/Components/Facebook/Facebook.js
@@ -4,6 +4,7 @@ const Facebook = () => {
 	return (
 		<div>
 			<iframe
+				title="facebook-page"
 				src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Frtshirtdesigns%2F&tabs=timeline&width=700&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
 				width="700"
 				height="500"
